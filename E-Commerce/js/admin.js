ef55594b8ffdbc0261e4b03eda86b1f3571fcbe6
sanjay k <130783511+sanjay7905@users.noreply.github.com>
2025-12-13 20:42:55 +0530

    Delete E-Commerce directory

--- E-Commerce/js/admin.js
+++
@@ -1,5 +0,0 @@
-
-function renderAdminProducts(){ 
-  if(window.renderAdminProducts) return window.renderAdminProducts();
-}
-function initProductForm(){ if(window.initProductForm) return window.initProductForm(); }
